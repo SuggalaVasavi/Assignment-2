bd6eb22993be67115f08be27db8f2665618b44aa
SuggalaVasavi <Suggala.vasa-scholar@hcl.com>
2017-06-01 19:15:56 +0530

    daily activity

--- JsonSimpleReadExample.java
+++ JsonSimpleReadExample.java
@@ -1,30 +1,38 @@
 
 
 import java.io.FileNotFoundException;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import java.text.ParseException;
+import java.util.Iterator;
 
 public class JsonSimpleReadExample {
 
-    public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
-    	JSONParser parser = new JSONParser();
-    	JSONObject obj=  (JSONObject) parser.parse(new FileReader("D:/Users/Suggala.vasa/workspace1/json/src/jsontxt.json"));
-    getKeyValue(obj,"paths");
-    }
-        static String getKeyValue(JSONObject obj , String key){
-        try {
-        	JSONObject jsonObject=obj;
-            //System.out.println(jsonObject);
-            System.out.println(jsonObject.get(key));
+	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
+		JSONParser parser = new JSONParser();
+		JSONObject obj=  (JSONObject) parser.parse(new FileReader("D:/Users/Suggala.vasa/workspace1/json/src/jsontxt.json"));
+		printJsonObject(obj,"paths");
+		Json(obj,"paths");
+	}
+	public  static void printJsonObject(JSONObject jsonObj,String keys) {
+		try{
+			Object keyvalue = jsonObj.get(keys);
+			if (keyvalue instanceof JSONObject){
+				System.out.println("key: "+ keys+"\n"); //Print key and value
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	public static void Json(JSONObject obj,String keys){
+		JSONObject posts = (JSONObject) obj.get(keys);
+		Iterator a = posts.keySet().iterator();
+		while(a.hasNext()) {
+			String key = (String)a.next();
+			// loop to get the dynamic key
+			System.out.print("key : "+key+"\n");
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-		return key;
-
-    }
+		}
+	}
 }
