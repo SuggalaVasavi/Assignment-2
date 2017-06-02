84cb82adf706068ccfff460ee62cc4b3b6011e87
SuggalaVasavi <Suggala.vasa-scholar@hcl.com>
2017-06-02 17:50:21 +0530

    final commit code for review

--- JsonSimpleReadExample.java
+++ JsonSimpleReadExample.java
@@ -1,38 +1,81 @@
+package json;
 
-
-import java.io.FileNotFoundException;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.text.ParseException;
-import java.util.Iterator;
+import java.io.PrintWriter;
+import java.util.Set;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 public class JsonSimpleReadExample {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
 		JSONParser parser = new JSONParser();
+		//parse file and taken as object
 		JSONObject obj=  (JSONObject) parser.parse(new FileReader("D:/Users/Suggala.vasa/workspace1/json/src/jsontxt.json"));
+		getKeyValue(obj,"paths");
+		getPath(obj,"paths");
 		printJsonObject(obj,"paths");
-		Json(obj,"paths");
+		saveFile(obj,"definitions");
 	}
-	public  static void printJsonObject(JSONObject jsonObj,String keys) {
+	static String getKeyValue(JSONObject obj , String key){
+		try {
+			JSONObject jsonObject=obj;
+			System.out.println(jsonObject.get(key));//basing on key value
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return key;
+
+	}
+
+	static String getPath(JSONObject obj , String key){
+		try {
+			JSONObject posts = (JSONObject) obj.get(key);
+			System.out.println(posts.keySet());//for getting list of "paths"
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return key;
+
+	}
+
+	static void printJsonObject(JSONObject jsonObj,String keys) {
+		JSONObject keyvalue = (JSONObject)jsonObj.get(keys);
+		//for nested objects iteration if required
+		if (keyvalue instanceof JSONObject){
+			printJsonObject(keyvalue, keys);
+
+			System.out.println("key: "+ keys + " value: " + keyvalue); //Print key and value
+		}
+	}
+	public static void saveFile(JSONObject obj,String keys)
+			throws IOException {
 		try{
-			Object keyvalue = jsonObj.get(keys);
-			if (keyvalue instanceof JSONObject){
-				System.out.println("key: "+ keys+"\n"); //Print key and value
+
+			JSONObject posts = (JSONObject) obj.get(keys);
+			// loop to get the dynamic key
+			Set<String> primeKeys = posts.keySet();
+			//changes set to String array
+			String a[]= primeKeys.toArray(new String[primeKeys.size()]);
+			for(int i = 0; i <a.length; i++) {
+				//converting array to String
+				String abc=a[i].toString();
+				// path for output file
+				FileWriter outFile = new FileWriter("D:/Users/Suggala.vasa/workspace1/json/src/pet/"+abc+".json");
+				PrintWriter out = new PrintWriter(outFile);
+				JSONObject keyvalue1 = (JSONObject) posts.get(abc);
+				//to print in the file
+				out.println(keyvalue1);
+				out.close();
 			}
-		}catch(Exception e){
+		}
+		catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	public static void Json(JSONObject obj,String keys){
-		JSONObject posts = (JSONObject) obj.get(keys);
-		Iterator a = posts.keySet().iterator();
-		while(a.hasNext()) {
-			String key = (String)a.next();
-			// loop to get the dynamic key
-			System.out.print("key : "+key+"\n");
-
-		}
-	}
 }
+
